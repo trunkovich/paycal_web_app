6609fe0a6f99f2bcf548cf1a91111069ce0d85e2
trunkovich <trunkovich@gmail.com>
2017-01-15 22:36:30 +0300

    added to home routes.

--- src/app/features/home/home.routes.ts
+++ src/app/features/home/home.routes.ts
@@ -2,8 +2,9 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from './home.component';
 import {OnlySignedInUsers} from '../../core/guards/only-signed-in-users.guard';
 import {OtherComponent} from './other.component';
+import {OnlyMobileDevices} from '../../core/guards/only-mobile-devices.guard';
 
 export const homeRoutes: Routes = [
-  { path: 'home', component: HomeComponent, canActivate: [OnlySignedInUsers] },
-  { path: 'other', component: OtherComponent, canActivate: [OnlySignedInUsers] }
+  { path: 'home', component: HomeComponent, canActivate: [OnlySignedInUsers, OnlyMobileDevices] },
+  { path: 'other', component: OtherComponent, canActivate: [OnlySignedInUsers, OnlyMobileDevices] }
 ];
