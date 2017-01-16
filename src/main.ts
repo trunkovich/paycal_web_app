381d6f66e1c51e86eb3868ba5a35252fb5ee2669
trunkovich <trunkovich@gmail.com>
2017-01-16 22:44:33 +0300

    enabled proxy-polyfill

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './polyfills.ts';
 import 'ts-helpers';
-// import 'proxy-polyfill';
+import 'proxy-polyfill';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
