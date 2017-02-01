e48b6497ff9c209c4aa12093f90bb3af73521720
trunkovich <trunkovich@gmail.com>
2017-02-01 12:03:03 +0300

    reworked auth.reducers

--- src/app/STATE/reducers/auth.reducer.ts
+++ src/app/STATE/reducers/auth.reducer.ts
@@ -2,7 +2,12 @@
  * Created by TrUnK on 06.01.2017.
  */
 import * as authActions from '../actions/auth.actions';
+import * as _ from 'lodash';
+
 import {TokenObject} from '../models/token.model';
+import {RequestPasswordRecoveryFailAction} from '../actions/auth.actions';
+import {ResetPasswordFailAction} from '../actions/auth.actions';
+import {SaveLeadFailAction} from '../actions/auth.actions';
 
 export interface AuthState {
   token: string | null;
@@ -27,86 +32,35 @@ export function authReducer(state: AuthState = initialAuthState, action: authAct
     case authActions.ActionTypes.RESET_PASSWORD:
     case authActions.ActionTypes.SAVE_LEAD:
     case authActions.ActionTypes.SIGN_IN: {
-      return {
-        token: state.token,
-        authenticated: state.authenticated,
-        redirectUrl: state.redirectUrl,
-        errorMsg: null,
-        loading: true
-      };
+      return setLoadingHanlder(state, true);
+    }
+    case authActions.ActionTypes.REQUEST_PASSWORD_RECOVERY_SUCCESS:
+    case authActions.ActionTypes.SIGN_IN_CLEAR_ERROR:
+    case authActions.ActionTypes.SAVE_LEAD_SUCCESS: {
+      return setLoadingHanlder(state, false);
     }
     case authActions.ActionTypes.REQUEST_PASSWORD_RECOVERY_FAIL:
     case authActions.ActionTypes.COMPLETE_REGISTRATION_FAIL:
     case authActions.ActionTypes.RESET_PASSWORD_FAIL:
     case authActions.ActionTypes.SAVE_LEAD_FAIL:
     case authActions.ActionTypes.SIGN_IN_FAIL: {
-      return {
-        token: state.token,
-        authenticated: state.authenticated,
-        redirectUrl: state.redirectUrl,
-        errorMsg: action.payload,
-        loading: false
-      };
+      return setErrorMsg(state, action);
     }
     case authActions.ActionTypes.COMPLETE_REGISTRATION_SUCCESS:
     case authActions.ActionTypes.SIGN_IN_SUCCESS: {
-      let tokenObject: TokenObject = action.payload;
-      return {
-        token: tokenObject.token,
-        authenticated: true,
-        redirectUrl: state.redirectUrl,
-        errorMsg: null,
-        loading: false
-      };
+      return signInHandler(state, action);
     }
     case authActions.ActionTypes.READ_TOKEN_SUCCESS: {
-      return {
-        token: action.payload,
-        authenticated: true,
-        redirectUrl: state.redirectUrl,
-        errorMsg: null,
-        loading: false
-      };
+      return readTokenHandler(state, action);
     }
     case authActions.ActionTypes.READ_TOKEN_FAIL: {
-      return {
-        token: null,
-        authenticated: false,
-        redirectUrl: state.redirectUrl,
-        errorMsg: null,
-        loading: false
-      };
-    }
-    case authActions.ActionTypes.REQUEST_PASSWORD_RECOVERY_SUCCESS:
-    case authActions.ActionTypes.SAVE_LEAD_SUCCESS: {
-      return {
-        token: state.token,
-        authenticated: state.authenticated,
-        redirectUrl: state.redirectUrl,
-        errorMsg: null,
-        loading: false
-      };
+      return readTokenFailHandler(state);
     }
     case authActions.ActionTypes.SAVE_REDIRECT_URL: {
-      return {
-        token: state.token,
-        authenticated: state.authenticated,
-        redirectUrl: action.payload,
-        errorMsg: null,
-        loading: false
-      };
+      return saveRedirectUrlHandler(state, action);
     }
     case authActions.ActionTypes.LOGOUT: {
-      return Object.assign({}, initialAuthState);
-    }
-    case authActions.ActionTypes.SIGN_IN_CLEAR_ERROR: {
-      return {
-        token: state.token,
-        authenticated: state.authenticated,
-        redirectUrl: state.redirectUrl,
-        errorMsg: null,
-        loading: false
-      };
+      return _.cloneDeep(initialAuthState);
     }
     default: {
       return state;
@@ -114,6 +68,74 @@ export function authReducer(state: AuthState = initialAuthState, action: authAct
   }
 }
 
+/* ------------------------------------------------------------------ */
+/* -------------------------REDUCER HANDLERS------------------------- */
+/* ------------------------------------------------------------------ */
+function setLoadingHanlder (state: AuthState, loadingState: boolean): AuthState {
+  let newState = _.cloneDeep(state);
+  newState.loading = loadingState;
+  newState.errorMsg = null;
+  return newState;
+}
+
+function setErrorMsg (state: AuthState, action: authActions.SignInFailAction |
+                                                authActions.CompleteRegistrationFailAction |
+                                                RequestPasswordRecoveryFailAction |
+                                                ResetPasswordFailAction |
+                                                SaveLeadFailAction): AuthState {
+  let newState = _.cloneDeep(state);
+  newState.loading = false;
+  newState.errorMsg = action.payload;
+  return newState;
+}
+
+function signInHandler(state: AuthState, action: authActions.SignInSuccessAction | authActions.CompleteRegistrationSuccessAction) {
+  let tokenObject: TokenObject = action.payload;
+  let newState = _.cloneDeep(state);
+
+  newState.token = tokenObject.token;
+  newState.authenticated = true;
+  newState.errorMsg = null;
+  newState.loading = false;
+
+  return newState;
+}
+
+function readTokenHandler(state: AuthState, action: authActions.ReadTokenSuccessAction) {
+  let newState = _.cloneDeep(state);
+
+  newState.token = action.payload;
+  newState.authenticated = true;
+  newState.errorMsg = null;
+  newState.loading = false;
+
+  return newState;
+}
+
+function readTokenFailHandler(state: AuthState) {
+  let newState = _.cloneDeep(state);
+
+  newState.token = null;
+  newState.authenticated = false;
+  newState.errorMsg = null;
+  newState.loading = false;
+
+  return newState;
+}
+
+function saveRedirectUrlHandler(state: AuthState, action: authActions.SaveRedirectUrl) {
+  let newState = _.cloneDeep(state);
+
+  newState.redirectUrl = action.payload;
+  newState.errorMsg = null;
+  newState.loading = false;
+
+  return newState;
+}
+
+/* ------------------------------------------------------------------- */
+/* -----------------------------SELECTORS----------------------------- */
+/* ------------------------------------------------------------------- */
 export const getToken = (state: AuthState) => state.token;
 export const getAuthenticated = (state: AuthState) => state.authenticated;
 export const getRedirectURL = (state: AuthState) => state.redirectUrl;
