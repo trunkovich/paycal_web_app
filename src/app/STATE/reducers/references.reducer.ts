b16f67ae2aa67c818427d54474e3c8a6a9781349
trunkovich <trunkovich@gmail.com>
2017-02-01 12:18:36 +0300

    changes in references reducer

--- src/app/STATE/reducers/references.reducer.ts
+++ src/app/STATE/reducers/references.reducer.ts
@@ -1,6 +1,8 @@
 /**
  * Created by TrUnK on 16.01.2017.
  */
+import * as _ from 'lodash';
+
 import * as referencesActions from '../actions/references.actions';
 import {GroupPosition} from '../models/group-position.model';
 import {GroupSpecialization} from '../models/group-specialization.model';
@@ -33,88 +35,49 @@ const initialReferencesState = {
 export function referencesReducer(state: ReferencesState = initialReferencesState, action: referencesActions.Actions): ReferencesState {
   switch (action.type) {
     case referencesActions.ActionTypes.LOAD_GROUP_POSITIONS_SUCCESS: {
-      return {
-        groupPositions: [...action.payload],
-        groupSpecializations: state.groupSpecializations,
-        regions: state.regions,
-        states: state.states,
-        timeZones: state.timeZones,
-        referencesTypes: state.referencesTypes,
-        employeeStatuses: state.employeeStatuses
-      };
+      return setReferenceItemHandler(state, action, 'groupPositions');
     }
     case referencesActions.ActionTypes.LOAD_GROUP_SPECIALIZATIONS_SUCCESS: {
-      return {
-        groupPositions: state.groupPositions,
-        groupSpecializations: [...action.payload],
-        regions: state.regions,
-        states: state.states,
-        timeZones: state.timeZones,
-        referencesTypes: state.referencesTypes,
-        employeeStatuses: state.employeeStatuses
-      };
+      return setReferenceItemHandler(state, action, 'groupSpecializations');
     }
     case referencesActions.ActionTypes.LOAD_REGIONS_SUCCESS: {
-      return {
-        groupPositions: state.groupPositions,
-        groupSpecializations: state.groupSpecializations,
-        regions: [...action.payload],
-        states: state.states,
-        timeZones: state.timeZones,
-        referencesTypes: state.referencesTypes,
-        employeeStatuses: state.employeeStatuses
-      };
+      return setReferenceItemHandler(state, action, 'regions');
     }
     case referencesActions.ActionTypes.LOAD_STATES_SUCCESS: {
-      return {
-        groupPositions: state.groupPositions,
-        groupSpecializations: state.groupSpecializations,
-        regions: state.regions,
-        states: [...action.payload],
-        timeZones: state.timeZones,
-        referencesTypes: state.referencesTypes,
-        employeeStatuses: state.employeeStatuses
-      };
+      return setReferenceItemHandler(state, action, 'states');
     }
     case referencesActions.ActionTypes.LOAD_TIME_ZONES_SUCCESS: {
-      return {
-        groupPositions: state.groupPositions,
-        groupSpecializations: state.groupSpecializations,
-        regions: state.regions,
-        states: state.states,
-        timeZones: [...action.payload],
-        referencesTypes: state.referencesTypes,
-        employeeStatuses: state.employeeStatuses
-      };
+      return setReferenceItemHandler(state, action, 'timeZones');
     }
     case referencesActions.ActionTypes.LOAD_REFERENCES_TYPES_SUCCESS: {
-      return {
-        groupPositions: state.groupPositions,
-        groupSpecializations: state.groupSpecializations,
-        regions: state.regions,
-        states: state.states,
-        timeZones: state.timeZones,
-        referencesTypes: [...action.payload],
-        employeeStatuses: state.employeeStatuses
-      };
+      return setReferenceItemHandler(state, action, 'referencesTypes');
     }
     case referencesActions.ActionTypes.LOAD_EMPLOYEE_STATUSES_SUCCESS: {
-      return {
-        groupPositions: state.groupPositions,
-        groupSpecializations: state.groupSpecializations,
-        regions: state.regions,
-        states: state.states,
-        timeZones: state.timeZones,
-        referencesTypes: state.referencesTypes,
-        employeeStatuses: [...action.payload]
-      };
+      return setReferenceItemHandler(state, action, 'employeeStatuses');
     }
     default: {
       return state;
     }
   }
 }
+/* ------------------------------------------------------------------ */
+/* -------------------------REDUCER HANDLERS------------------------- */
+/* ------------------------------------------------------------------ */
+function setReferenceItemHandler(state: ReferencesState, action: referencesActions.LoadGroupPositionsSuccessAction |
+                                                                referencesActions.LoadEmployeeStatusesSuccessAction |
+                                                                referencesActions.LoadGroupSpecializationsSuccessAction |
+                                                                referencesActions.LoadReferencesTypesSuccessAction |
+                                                                referencesActions.LoadRegionsSuccessAction |
+                                                                referencesActions.LoadStatesSuccessAction |
+                                                                referencesActions.LoadTimeZonesSuccessAction, key: string) {
+  let newState = _.cloneDeep(state);
+  newState[key] = _.cloneDeep(action.payload);
+  return newState;
+}
 
+/* ------------------------------------------------------------------- */
+/* -----------------------------SELECTORS----------------------------- */
+/* ------------------------------------------------------------------- */
 export const getGroupPositions = (state: ReferencesState) => state.groupPositions;
 export const getGroupSpecializations = (state: ReferencesState) => state.groupSpecializations;
 export const getRegions = (state: ReferencesState) => state.regions;
