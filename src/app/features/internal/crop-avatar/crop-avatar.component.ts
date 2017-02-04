d829a0c1cf55aa24afc511c2c4cdc522a9927129
trunkovich <trunkovich@gmail.com>
2017-02-04 16:34:50 +0300

    added photo to crop.

--- src/app/features/internal/crop-avatar/crop-avatar.component.ts
+++ src/app/features/internal/crop-avatar/crop-avatar.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Component, OnInit, OnDestroy, ElementRef} from '@angular/core';
 import {AvatarService} from '../../../core/services/avatar.service';
 import {INTERNAL_ROUTES} from '../internal.routes';
 import {Router} from '@angular/router';
@@ -7,6 +7,7 @@ import {Store} from '@ngrx/store';
 import {Subscription, Observable} from 'rxjs';
 import {ImageDataModel} from '../../../STATE/models/image-data.model';
 import {Crop} from './crop.class';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'pcl-crop-avatar',
@@ -15,15 +16,22 @@ import {Crop} from './crop.class';
 })
 export class CropAvatarComponent implements OnInit, OnDestroy {
   crop: Crop;
+  private container: HTMLElement;
   private sub: Subscription;
 
-  constructor(private avatarService: AvatarService, private router: Router, private store: Store<AppState>) {}
+  constructor(
+    private location: Location,
+    private elRef: ElementRef,
+    private router: Router,
+    private store: Store<AppState>
+  ) {}
 
   ngOnInit() {
     this.sub = this.store.select(profileSelectors.getUploadedImageData)
       .subscribe((data: ImageDataModel) => {
         if (data) {
-          this.crop = new Crop(data)
+          this.container = <HTMLElement>this.elRef.nativeElement.getElementsByClassName('crop-container')[0];
+          this.crop = new Crop(data, this.container.offsetWidth, this.container.offsetHeight);
           setTimeout(() => this.sub.unsubscribe(), 0);
         } else {
           this.router.navigate(['/', INTERNAL_ROUTES.EDIT_PROFILE]);
@@ -37,6 +45,12 @@ export class CropAvatarComponent implements OnInit, OnDestroy {
     }
   }
 
+  onBackBtnClick() {
+    this.location.back();
+  }
 
+  save() {
+    console.log('save clicked');
+  }
 
 }
--- src/app/features/internal/crop-avatar/crop.class.ts
+++ src/app/features/internal/crop-avatar/crop.class.ts
@@ -3,16 +3,45 @@
  */
 import {ImageDataModel} from '../../../STATE/models/image-data.model';
 
+interface Coords {
+  x: number;
+  y: number;
+}
+
 export class Crop {
   data: ImageDataModel;
+  viewWidth: number;
+  viewHeight: number;
+  photoAreaWidth: number;
+  photoAreaHeight: number;
+  imageAspectRatio: number;
+  viewAspectRatio: number;
+  topLeft: Coords;
 
-  constructor(imageData: ImageDataModel) {
+  constructor(imageData: ImageDataModel, viewWidth: number, viewHeight: number) {
     this.data = imageData;
+    this.viewWidth = viewWidth;
+    this.viewHeight = viewHeight;
+    this.viewAspectRatio = this.viewWidth / this.viewHeight;
+    this.imageAspectRatio = this.data.width / this.data.height;
+    if (this.imageAspectRatio > this.viewAspectRatio) {
+      this.photoAreaWidth = this.viewWidth;
+      this.photoAreaHeight = this.data.height / (this.data.width / this.viewWidth);
+      this.topLeft = {x: 0, y: (this.viewHeight - this.photoAreaHeight) / 2};
+    } else {
+      this.photoAreaHeight = this.viewHeight;
+      this.photoAreaWidth = this.data.width / (this.data.height / this.viewHeight);
+      this.topLeft = {y: 0, x: (this.viewWidth - this.photoAreaWidth) / 2};
+    }
   }
 
   getImageStyles(): any {
     if (this.data) {
       return {
+        'width': this.photoAreaWidth + 'px',
+        'height': this.photoAreaHeight + 'px',
+        'top': this.topLeft.y + 'px',
+        'left': this.topLeft.x + 'px',
         'background-image': 'url(' + this.data.dataUri + ')'
       };
     } else {
