7dd7d8494beee2988fdbfc6d6989aa3ef4f479f1
trunkovich <trunkovich@gmail.com>
2017-02-08 22:27:52 +0300

    added final redirect.

--- src/app/features/internal/crop-loading/crop-loading.component.ts
+++ src/app/features/internal/crop-loading/crop-loading.component.ts
@@ -8,4 +8,16 @@ import {Router} from '@angular/router';
   templateUrl: './crop-loading.component.html',
   styleUrls: ['./crop-loading.component.scss']
 })
-export class CropLoadingComponent {}
+export class CropLoadingComponent implements OnInit, OnDestroy {
+  timeoutId;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.timeoutId = setTimeout(() => this.router.navigate(['/', INTERNAL_ROUTES.PROFILE]), APP_CONFIG.AUTO_REDIRECT_TIMER * 10);
+  }
+
+  ngOnDestroy() {
+    clearTimeout(this.timeoutId);
+  }
+}
