66ce3e8d4fa2b14549650fb88f9c1784f6074783
trunkovich <trunkovich@gmail.com>
2017-06-24 13:55:16 +0300

    First version of integration

---
+++ src/app/features/internal/google-form/google-form.component.ts
@@ -0,0 +1,19 @@
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'pcl-google-form',
+  templateUrl: './google-form.component.html',
+  styleUrls: ['./google-form.component.scss']
+})
+export class GoogleFormComponent implements AfterViewInit {
+  @ViewChild('iframe') iframe: ElementRef;
+
+  constructor(private elementRef: ElementRef) { }
+
+  ngAfterViewInit() {
+    const doc = this.iframe.nativeElement.contentWindow.document;
+    doc.open();
+    doc.write('<style>body{margin: 0}</style><script type="text/javascript" src="https://Paycal.formstack.com/forms/js.php/pmt"></script>');
+    doc.close();
+  }
+}
