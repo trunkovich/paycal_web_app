55df8cb60e00489676067217b24d7de181524a4e
trunkovich <trunkovich@gmail.com>
2018-02-04 10:30:30 +0300

    Fix only mobile issue

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import { OnlyDesktopDevices } from './core/guards/only-desktop-devices.guard';
+import { OnlyMobileComponent } from './core/components/only-mobile/only-mobile.component';
 
 export const AppRoutes: Routes = [
-  // { path: 'only-mobile', component: OnlyMobileComponent, canActivate: [OnlyDesktopDevices] },
+  { path: 'only-mobile', component: OnlyMobileComponent, canActivate: [OnlyDesktopDevices] },
   // { path: '**', pathMatch: 'full', redirectTo: '/home' },
 ];
