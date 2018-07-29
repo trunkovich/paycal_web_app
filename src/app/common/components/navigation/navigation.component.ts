10a40bc423a6dc1e87d8e962537293b3252ad6be
trunkovich <trunkovich@gmail.com>
2018-07-29 20:34:22 +0300

    Add offline message

--- src/app/common/components/navigation/navigation.component.ts
+++ src/app/common/components/navigation/navigation.component.ts
@@ -7,17 +7,52 @@ import { BottomSheetService } from '../../../bottom-sheet/bottom-sheet.service';
 import { ContactUsBottomSheetComponent } from '../../../features/internal/contact-us-bottom-sheet/contact-us-bottom-sheet.component';
 import { map } from 'rxjs/operators';
 import { Employee } from '../../../STATE/models/employee.model';
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'pcl-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
+  animations: [
+    trigger('offlineBlock', [
+      state('hide', style({
+        transform: 'translate3d(0, 100px, 0)'
+      })),
+      state('show-above-nav',   style({
+        transform: 'translate3d(0, 0, 0)'
+      })),
+      state('show',   style({
+        transform: 'translate3d(0, 50px, 0)'
+      })),
+      transition('hide => show', animate('200ms ease-out')),
+      transition('hide => show-above-nav', animate('200ms ease-out')),
+      transition('show => show-above-nav', animate('200ms ease-out')),
+      transition('show-above-nav => show', animate('200ms ease-out')),
+      transition('show-above-nav => hide', [
+        animate(2000, keyframes([
+          style({backgroundColor: '#25252b', offset: 0}),
+          style({backgroundColor: '#28a745', offset: 0.1}),
+          style({backgroundColor: '#28a745', offset: 0.9}),
+          style({backgroundColor: '#28a745', transform: 'translate3d(0, 100px, 0)',  offset: 1.0})
+        ]))
+      ]),
+      transition('show => hide', [
+        animate(2000, keyframes([
+          style({backgroundColor: '#25252b', offset: 0}),
+          style({backgroundColor: '#28a745', offset: 0.1}),
+          style({backgroundColor: '#28a745', offset: 0.9}),
+          style({backgroundColor: '#28a745', transform: 'translate3d(0, 100px, 0)',  offset: 1.0})
+        ]))
+      ])
+    ])
+  ]
 })
 export class NavigationComponent implements OnChanges {
   @Input() show: boolean;
   @Input() online: boolean;
   isHomeDisabled$: Observable<boolean>;
-  @HostBinding('class.show') someNav = false;
+  showNav = false;
+  offlineBlockStatus: 'hide' | 'show' | 'show-above-nav' = 'hide';
 
   constructor(private store: Store<AppState>, private bss: BottomSheetService) {
     this.isHomeDisabled$ = store.select(profileSelectors.getMyProfile)
@@ -31,7 +66,8 @@ export class NavigationComponent implements OnChanges {
   }
 
   ngOnChanges() {
-    this.someNav = this.show;
+    this.showNav = this.show;
+    this.offlineBlockStatus = this.online ? 'hide' : this.showNav ? 'show-above-nav' : 'show';
   }
 
 }
